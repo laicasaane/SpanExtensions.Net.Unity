8275e7321aa41dd043b4b7577fbc2167f0e1a36b
dragon7307 <tim.clemens0703@gmail.com>
2025-09-28 16:44:45 +0200

    vectorized Min()

--- tests/unit-tests/Tests/ReadOnlySpan/Linq/LinqMinMaxTests.cs
+++ tests/unit-tests/Tests/ReadOnlySpan/Linq/LinqMinMaxTests.cs
@@ -1,6 +1,6 @@
 ﻿namespace SpanExtensions.Tests.UnitTests
 {
-    public class LinqMaxTests
+    public class LinqMinMaxTests
     {
         [Theory]
         [MemberData(nameof(GetMaxData))]
@@ -8,6 +8,14 @@
         {
             ReadOnlySpan<int> span = source.AsSpan();
             Assert.Equal(max, span.Max());
+        }
+
+        [Theory]
+        [MemberData(nameof(GetMinData))]
+        public void Min(int[] source, int min)
+        {
+            ReadOnlySpan<int> span = source.AsSpan();
+            Assert.Equal(min, span.Min());
         }
 
         public static TheoryData<int[], int> GetMaxData()
@@ -25,6 +33,25 @@
             data.Add(Samples10, max10);
             data.Add(Samples100, max100);
             data.Add(Samples1000, max1000);
+
+            return data;
+        }
+
+        public static TheoryData<int[], int> GetMinData()
+        {
+            var data = new TheoryData<int[], int>();
+
+            var Samples10 = GetSampleSetInts(10);
+            var Samples100 = GetSampleSetInts(100);
+            var Samples1000 = GetSampleSetInts(1000);
+
+            int min10 = Samples10.Min();
+            int min100 = Samples100.Min();
+            int min1000 = Samples1000.Min();
+
+            data.Add(Samples10, min10);
+            data.Add(Samples100, min100);
+            data.Add(Samples1000, min1000);
 
             return data;
         }
